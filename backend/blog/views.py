5cefe8c5902ece661b34d6de669ec1ae362c958f
paulo <maicon271271@gmail.com>
2023-11-17 11:22:19 -0400

    modified:   backend/blog/views.py 	new file:   "exten\303\247\303\265es.txt"

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -9,12 +9,12 @@ from .serializers import PostSerializer
 
 class BlogListView(APIView):
     def get(self, request, *args, **kwargs):
-        posts = Post.objects.all()[0:4] #[0:4] isso e a paginação
+        posts = Post.objects.all()[0:4] # TODO: [0:4] isso e a paginação
         serializers = PostSerializer(posts, many=True)
         return Response(serializers.data)
 
 class PostDetailView(APIView):
         def get(self, request, post_slug, *args, **kwargs):
             post = get_list_or_404(Post, slug=post_slug)
-            serializers = PostSerializer(post, many=True)
+            serializers = PostSerializer(post, many=True) # TODO: oque e many=True
             return Response(serializers.data)
